793cd312b4210240a52980134fd343bd5707196b
RomanosEf24 <romaeutu@gmail.com>
2025-11-24 14:28:55 +0200

    Δημιουργια-user-και-ελεγχος-backend

---
+++ cinedaltonsproj/src/main/java/com/cinedaltons/entity/Usre.java
@@ -0,0 +1,28 @@
+package com.movieapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true) // SOS: Απαγορεύει τα διπλότυπα στη βάση
+    private String username;
+
+    private String password;
+    private String email;
+
+    // Getters and Setters
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
