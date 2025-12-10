c3a99c458600fbbb066be90bf560ff865418ff27
bobDaBuildest <152222986+bobDaBuildest@users.noreply.github.com>
2025-12-10 19:12:00 +0200

    added soupabase

--- cinedaltonsproj/src/main/java/com/cinedaltons/dto/UserRegisterDTO.java
+++ cinedaltonsproj/src/main/java/com/cinedaltons/dto/UserRegisterDTO.java
@@ -6,7 +6,6 @@ public class UserRegisterDTO {
     private String email;
     private String name;
     private String surname;
-    private String birthday;
 
     //Getters and Setters
     public String getUsername() {return username;}
@@ -22,6 +21,4 @@ public class UserRegisterDTO {
     public void setName(String name) {this.name = name;}
     public String getSurname() {return surname;}
     public void setSurname(String surname) {this.surname = surname;}
-    public String getBirthday() {return birthday;}
-    public void setBirthday(String birthday) {this.birthday = birthday;}
 }
